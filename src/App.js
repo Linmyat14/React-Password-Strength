7ebca7ee8665d6edeeace29cb5caa36937bea3a9
Linmyat14 <linmyat.n3@gmail.com>
2021-10-08 22:34:46 +0630

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component, useState } from "react";
+import PasswordStrengthMeter from "./components/PasswordStringMeter";
+function App () {
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className="container">
+      <div className="col-md-6 mx-auto text-right">
+        <h3 className="text-center my-5">Cand Dev</h3>
+        <div className="form-group mb-1">
+          <input 
+          type="password" 
+          className="form-control shadow-none" 
+          placeholder="Password"
+          onChange={e => setPassword(e.target.value)}
+          />
+         </div>
+         <PasswordStrengthMeter password={password} />
+      </div>
+    </div>
+  )
+}
+
+export default App;
